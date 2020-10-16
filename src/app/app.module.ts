17859b3ba921dd6066548c68a34651b93d33abb7
Dmitry Filkov <dmitry.filkov@web.de>
2020-10-16 21:02:25 +0200

    added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxGraphModule } from '@swimlane/ngx-graph';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -22,7 +23,8 @@ import { MainViewComponent } from './main-view/main-view.component';
         BrowserAnimationsModule,
         AppRoutingModule,
         NgxGraphModule,
-        HttpClientModule
+        HttpClientModule,
+        NgbModule
     ],
     providers: [],
     bootstrap: [AppComponent]
