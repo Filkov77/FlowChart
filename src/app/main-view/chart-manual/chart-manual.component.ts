022ee8f7a2350115a787c8d6f4c94d2e570a4d20
Dmitry Filkov <dmitry.filkov@web.de>
2020-11-05 20:04:17 +0100

    arrows

--- src/app/main-view/chart-manual/chart-manual.component.ts
+++ src/app/main-view/chart-manual/chart-manual.component.ts
@@ -1,12 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ElementRef, HostListener, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { ClusterNode, Edge, Node } from '@swimlane/ngx-graph';
+import { getArrow } from 'perfect-arrows';
 
 @Component({
     selector: 'app-chart-manual',
     templateUrl: './chart-manual.component.html',
     styleUrls: ['./chart-manual.component.scss']
 })
-export class ChartManualComponent {
+export class ChartManualComponent implements AfterViewInit {
+
+    @ViewChild('one')
+    one!: ElementRef<HTMLDivElement>;
+
+    @ViewChild('two')
+    two!: ElementRef<HTMLDivElement>;
+
+    @ViewChild('three')
+    three!: ElementRef<HTMLDivElement>;
+
+    @ViewChild('four')
+    four!: ElementRef<HTMLDivElement>;
+
     @Input() nodes!: Node[];
 
     // nodes = [{
@@ -27,6 +41,55 @@ export class ChartManualComponent {
     // edges = [{ 'source': '1', 'target': '2', 'id': 'a0tf5' }, { 'source': '1', 'target': '3', 'id': 'aqbjn' }];
 
     @Input() clusters!: ClusterNode[];
+
+    path: string | undefined;
+    ex = 0;
+    ey = 0;
+    sx = 0;
+    sy = 0;
+    endAngleAsDegrees = 0;
+
+    ngAfterViewInit() {
+        setTimeout(() => {
+            this.initArrow();
+        });
+    }
+
+    @HostListener('window:resize', [])
+    private onResize() {
+        this.initArrow();
+    }
+
+    private initArrow() {
+        const firstEl = this.one.nativeElement.getBoundingClientRect();
+        const secondEl = this.two.nativeElement.getBoundingClientRect();
+        const p1 = { x: firstEl.x + firstEl.width, y: (firstEl.y + firstEl.height / 2) };
+        const p2 = { x: secondEl.x, y: (secondEl.y + secondEl.height / 2) };
+
+        const arrow = getArrow(
+            p1.x,
+            p1.y,
+            p2.x,
+            p2.y,
+            {
+                bow: 0,
+                stretch: 0,
+                padEnd: 6
+            }
+        );
+
+        const [sx, sy, cx, cy, ex, ey, ae, as, ec] = arrow;
+        this.ex = ex;
+        this.ey = ey;
+        this.sx = sx;
+        this.sy = sy;
+
+        this.endAngleAsDegrees = ae * (180 / Math.PI);
+
+        this.path = `M${sx},${sy} Q${cx},${cy} ${ex},${ey}`;
+    }
+
+
     // clusters = [{ 'id': 'c2', 'childNodeIds': ['2', '3'], label: '' }];
     // clusters = [];
 }
